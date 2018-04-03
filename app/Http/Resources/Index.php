cf2ec51c47051e0c99773322d9f58daa405ab533
legolas-le <ngonthapvinhhang@gmail.com>
2018-04-03 22:28:09 +0700

    [initialize] application

---
+++ app/Http/Resources/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tutor_Van\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Index extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
