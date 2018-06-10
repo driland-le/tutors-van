12d2b3927fa0ec06feaf072cabe7024f5fed9f1d
legolas-le <ngonthapvinhhang@gmail.com>
2018-06-10 19:09:54 +0700

    [template-css] fix common template

--- resources/views/site/partials/_common.blade.php
+++ resources/views/site/partials/_common.blade.php
@@ -1,6 +1,6 @@
 @section('title', 'gia sư sư phạm tp.HCM')
 @section('banner')
-<img src="images/banner/education-hub-banner-3.jpg" alt="gia sư sư phạm hồ chí minh" style="width: 100%;">
+  <img src="images/banner/education-hub-banner-3.jpg" alt="gia sư sư phạm hồ chí minh" style="width: 100%;">
 @endsection
 @section('sidebar')
 <div class="slide"></div>
